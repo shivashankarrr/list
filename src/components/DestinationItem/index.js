9cd68d241cfc1e6b62b790c027cfa4f12f4f26d0
shiva <shankuu786@gmail.com>
2023-09-11 04:48:07 +0000

    error

---
+++ src/components/DestinationItem/index.js
@@ -0,0 +1,14 @@
+// Write your code here
+import './index.css'
+
+const DestinationItem = props => {
+  const {destinationDetails} = props
+  const {name, imgUrl} = destinationDetails
+  return (
+    <li className="destination-item">
+      <img src={imgUrl} alt={name} className="image" />
+      <p className="main-heading">{name}</p>
+    </li>
+  )
+}
+export default DestinationItem
